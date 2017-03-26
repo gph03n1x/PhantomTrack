e09047451943d013425ea6be203d38ea1ff9190e
gph03n1x <giannispapcod7@gmail.com>
2017-03-27 01:34:09 +0300

    Now the widgets takes into consideration when looking for a cached song the bars and the steps that were used for the sampling.

--- core/analysis/widget.py
+++ core/analysis/widget.py
@@ -20,45 +20,13 @@ class WaveGraphic(QWidget):
         self.bars = 60
         self.step = 250
         self.between = 10
+        self.data = None
         self.data_to_animate = None
         self.setFixedHeight((self.geometry().height()-220)/2)
         self.storage = Storage()
         #self.stop = False
         self.timer = QTimer()
         self.timer.timeout.connect(self.animate)
-
-    def set_title(self, title):
-        self.parent().parent().setWindowTitle("Phantom Track "+title)
-
-    def is_song_cached(self, song):
-        self.storage.cur.execute("select count(*) from song_info where song_hash = ?", (self.get_song_hash(song),))
-        return self.storage.cur.fetchone()[0] != 0
-
-    def get_song_hash(self, song):
-        song = song.rsplit(".", 1)[0]
-        return hashlib.sha224(song.encode()).hexdigest()
-
-    def load_waves(self, song):
-        if not self.is_song_cached(song):
-            print("Attempt to read non-cached song")
-            return
-
-        song_hash = self.get_song_hash(song)
-
-        self.storage.cur.execute("select * from song_info where song_hash = ?", (song_hash,))
-        results = self.storage.cur.fetchone()
-        self.data = results[1]
-
-        self.starting_point = 0
-        self.set_title(song)
-
-    def cache_waves(self, song, data, duration):
-        if not self.is_song_cached(song):
-            song_hash = self.get_song_hash(song)
-            self.storage.cur.execute("insert into song_info (song_hash, wave_form, duration, bars, step) values (?,?,?,?,?)",
-                                     (song_hash, data, duration, self.bars, self.step))
-            self.storage.con.commit()
-
 
     def stop(self):
         self.timer.stop()
@@ -73,7 +41,45 @@ class WaveGraphic(QWidget):
         self.timer.stop()
         self.hide()
 
+    def set_title(self, title):
+        self.parent().parent().setWindowTitle("Phantom Track "+title)
+
+    def get_song_hash(self, song):
+        song = song.rsplit(".", 1)[0]
+        return hashlib.sha224(song.encode()).hexdigest()
+
+    def is_song_cached(self, song):
+        self.storage.cur.execute("select count(*) from song_info where song_hash = ? AND bars = ? AND step = ?",
+                                 (self.get_song_hash(song), self.bars, self.step))
+
+        return self.storage.cur.fetchone()[0] != 0
+
+    def load_waves(self, song):
+        if not self.is_song_cached(song):
+            print("Attempt to read non-cached song")
+            return
+
+        song_hash = self.get_song_hash(song)
+
+        self.storage.cur.execute("select * from song_info where song_hash = ? AND bars = ? AND step = ?",
+                                 (song_hash, self.bars, self.step))
+        results = self.storage.cur.fetchone()
+        self.data = results[1]
+
+        self.starting_point = 0
+        self.set_title(song)
+
+    def cache_waves(self, song, data, duration):
+        if not self.is_song_cached(song):
+            song_hash = self.get_song_hash(song)
+            self.storage.cur.execute("insert into song_info (song_hash, wave_form, duration, bars, step) values (?,?,?,?,?)",
+                                     (song_hash, data, duration, self.bars, self.step))
+            self.storage.con.commit()
+
     def animate(self):
+        #print("called")
+        #print(len(self.data))
+        #print(len(self.data)/self.bars)
         if self.data is None:
             self.timer.start(self.step)
             return
@@ -119,9 +125,9 @@ class WaveGraphic(QWidget):
         qp.end()
 
     def set_wav(self, wav, title):
-        self.store_wav_info(wav, title)
+        self.sample_wave_file(wav)
 
-    def store_wav_info(self, wave_file, file_name):
+    def sample_wave_file(self, wave_file):
         with contextlib.closing(wave.open(wave_file, 'r')) as f:
             frames = f.getnframes()
             rate = f.getframerate()
@@ -130,8 +136,8 @@ class WaveGraphic(QWidget):
         wave_data = read(wave_file)[1]
         os.remove(wave_file)
 
-        rate = int(len(wave_data) / (duration * self.bars * (self.step / 1000)))
+        rate = int(len(wave_data) * (self.step / 1000) / (duration * self.bars ))
 
         self.data = wave_data[::rate]
         self.cache_waves(wave_file, self.data, duration)
-        self.load_waves(file_name)
+        self.load_waves(wave_file)
